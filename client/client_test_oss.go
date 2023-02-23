7089da21894033d39297708a219b413e530dd9e5
RhinoUser0 <rhino_fs@163.com>
2023-02-23 10:32:04 +0800

    Fix a set of none sense stuff

--- client/client_test_oss.go
+++ client/client_test_oss.go
@@ -1,39 +1,36 @@
 package main
 
 import (
-        "bufio"
-        "log"
-        "os"
-        "os/exec"
-
-        "go.uber.org/zap"
+	"bufio"
+	"log"
+	"os"
+	"os/exec"
 )
 
-func go_test_exec(url string){
-        cmd := exec.Command("wget", "http://localhost:10009/getFile?url="+url)
-        cmd.Stdout = os.Stdout
-        cmd.Stderr = os.Stderr
-        cmd.Dir = "/home/sqyan/"
-        err := cmd.Run()
-        if err != nil {
-                log.Printf("failed to call cmd.Run(): %v\n", err)
-        }
+func go_test_exec(url string) {
+	cmd := exec.Command("wget", "http://localhost:10008/getFile?url="+url)
+	cmd.Stdout = os.Stdout
+	cmd.Stderr = os.Stderr
+	cmd.Dir = "/home/xxx/"
+	err := cmd.Run()
+	if err != nil {
+		log.Printf("failed to call cmd.Run(): %v\n", err)
+	}
 }
 
 func main() {
-        var zapLogger *zap.Logger
-        var urlSet []string
-        urlPath := "/home/sqyan/urlPath.txt"
-        file, err := os.Open(urlPath)
-        if err != nil {
-                zapLogger.Debug("Error when opening files!")
-        }
-        fileScanner := bufio.NewScanner(file)
-        for fileScanner.Scan() {
-                urlSet = append(urlSet, fileScanner.Text())
-        }
-        file.Close()
-        for _, url := range urlSet {
-            go_test_exec(url)
-        }
+	var urlSet []string
+	urlPath := "/home/xxx/urlPath.txt"
+	file, err := os.Open(urlPath)
+	if err != nil {
+		log.Printf("Error when opening files!")
+	}
+	fileScanner := bufio.NewScanner(file)
+	for fileScanner.Scan() {
+		urlSet = append(urlSet, fileScanner.Text())
+	}
+	file.Close()
+	for _, url := range urlSet {
+		go_test_exec(url)
+	}
 }

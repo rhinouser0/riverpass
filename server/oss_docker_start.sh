923a3466f7ca5de734181d3d21a4a0e52c64eb18
LYAccc <49501068+LYAccc@users.noreply.github.com>
2023-08-04 17:09:31 +0800

    Update oss_docker_start.sh

--- server/oss_docker_start.sh
+++ server/oss_docker_start.sh
@@ -3,5 +3,5 @@ cur_dir=$(pwd)"/data"
 sudo docker run -itd --name oss-mysql -p 3310:3306 -v $cur_dir:/var/lib/mysql -e MYSQL_ROOT_PASSWORD=123456 oss-mysql
 sleep 1s
 echo "start golang container ..."
-sudo docker run -p 10009:10008  --name oss-go-docker -v /tmp/localfs_oss:/tmp/localfs_oss --link oss-mysql -e max_size=$1 riverpass
+sudo docker run -p 10009:10008  --name oss-go-docker -v $(pwd)"/localfs_oss":/tmp/localfs_oss --link oss-mysql -e max_size=$1 riverpass
 echo "finish"
